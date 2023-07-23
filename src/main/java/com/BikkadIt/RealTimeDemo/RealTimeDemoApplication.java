7e6db5e3c1540a0973efb806704a6c5d2e39f706
BhagyashriD94 <bhagyashrideshmane@gmail.com>
2023-07-23 06:20:38 +0530

    This is first project on RealTimeDemo

---
+++ src/main/java/com/BikkadIt/RealTimeDemo/RealTimeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.RealTimeDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BikkadIt/RealTimeDemo/RealTimeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.RealTimeDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
